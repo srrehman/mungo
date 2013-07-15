60a7ca235a26c28703a7d72471224c7e359552d7
Kerby Martino <kerbymart@gmail.com>
2013-07-16 01:00:51 +0800

    Test model updates

--- src/test/java/com/mungoae/models/User.java
+++ src/test/java/com/mungoae/models/User.java
@@ -1,12 +1,17 @@
 package com.mungoae.models;
 
+import com.mungoae.annotations.Entity;
 import com.mungoae.annotations.Id;
 
+@Entity
 public class User {
 	@Id
 	private String id;
 	private String username;
 	public User(){}
+	public User(String username){
+		this.username = username;
+	}
 	public String getUsername() {
 		return username;
 	}
